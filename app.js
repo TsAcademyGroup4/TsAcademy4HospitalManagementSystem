17e493dc580618bd8c28493d9e1ac07ee62fac65
Othniel <chukchi2345@gmail.com>
2026-02-19 15:44:19 +0100

    Changed to module import, imported route and database connection function, add route for authController, created a startapplication function for connecting the database and starting the application

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import "dotenv/config";
+import loginRoute from "./routes/auth.routes.js";
+import { connectDB } from "./db/config/database.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 
 // ----------------------
 // Middleware
@@ -12,13 +15,26 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
+// Routes
+app.use("/api/auth", loginRoute);
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to TsAcademy Group 4 Project' });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to TsAcademy Group 4 Project" });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+async function startApplication() {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start application:", error.message);
+    process.exit(1);
+  }
+}
+
+startApplication();
