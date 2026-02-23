efe10d9dc0830ea2a6debc741076836704777d73
Othniel <chukchi2345@gmail.com>
2026-02-23 14:41:58 +0100

    Reconfigured app: Made function to start app, import db connection function, invoked db connection function

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
-dotenv.config()
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import { connectDB } from "./db/config/database";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,24 +15,29 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // ----------------------
 // Routes
 // ----------------------
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to TsAcademy Group 4 Project' });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to TsAcademy Group 4 Project" });
 });
 
 // ----------------------
 // Start server
 // ----------------------
-const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
 
-// Handle server errors
-server.on('error', (err) => {
-    console.error('Server failed to start:', err);
-    process.exit(1); // Exit with failure if server cannot start
-});
+async function startApplication() {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server failed to start:", error);
+    process.exit(1);
+  }
+}
+
+startApplication();
