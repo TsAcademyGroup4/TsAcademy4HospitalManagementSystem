bedc1d94121a9f0ab2ed4420ebfb240931924486
TsAcademyGroup4 <azuchukwuemeka9@gmail.com>
2026-03-06 16:38:59 -0800

    Modified appointmentService
    removed working hours array to make appointment creation more dynamic

--- services/appointmentService.js
+++ services/appointmentService.js
@@ -1,15 +1,5 @@
 import appointmentRepository from "../repositories/appointment.repository.js";
 
-<<<<<<< Updated upstream
-const WORKING_HOURS = [
-  "09:00","09:30","10:00","10:30",
-  "11:00","11:30","12:00","12:30",
-  "13:00","13:30","14:00","14:30",
-  "15:00","15:30","16:00","16:30"
-];
-
-=======
->>>>>>> Stashed changes
 // helper to convert HH:MM into minutes
 const toMinutes = (t) => {
   const [h, m] = t.split(":").map(Number);
