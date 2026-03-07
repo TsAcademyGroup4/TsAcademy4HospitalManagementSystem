3b93ab4801e170e39c8c29b93ad35130fb746c32
TsAcademyGroup4 <azuchukwuemeka9@gmail.com>
2026-03-07 07:34:14 -0800

    Fixed Missing import of entryController in entryRoute

--- routes/entryRoutes.js
+++ routes/entryRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import * as entryController from "../controllers/entryController.js";
 
 const router = express.Router();
 router.get("/", entryController.greetingMessage);
